f955a0a8644f102cdcf2b51ee73b54c9286fafb7
Yunus Hossain <yonusprince@gmail.com>
2025-02-20 17:47:14 -0500

    created function to get polybius key that theoretically works

--- ciphers/nihilist_cipher_solver.py
+++ ciphers/nihilist_cipher_solver.py
@@ -1,3 +1,4 @@
+from string import ascii_lowercase as lowercase_letters
 from cipher_solver import CipherSolver
 
 class NihilistSolver(CipherSolver):
@@ -7,9 +8,30 @@ class NihilistSolver(CipherSolver):
     def encode(self, plaintext, keyword = None):
         return super().encode(plaintext, keyword)
     
-    def get_polybius_key(keyword : str) -> list:
-        polybius_key = [[], [], [], [], []]
-        for row in polybius_key:
+    def get_polybius_key(self, keyword : str) -> list:
+        polybius_square = [[], [], [], [], []]
+        keyword_index_left_on = 0
+        letters_index_left_on = 0
+        for row in polybius_square:
             if len(row) >= 5:
                 pass
+            elif keyword_index_left_on != len(keyword) - 1:
+                for index, letter in enumerate(keyword[keyword_index_left_on + 1:]):
+                    if not self.letter_is_in_polybius_square(polybius_square):
+                        row.append(letter)
+                        keyword_index_left_on = index
+            else:
+                for index, letter in enumerate(lowercase_letters[letters_index_left_on + 1:]):
+                    if not self.letter_is_in_polybius_square(polybius_square):
+                        row.append(letter)
+                        letters_index_left_on = index 
+            
+        
+    
+    def letter_is_in_polybius_square(self, polybius_square : list, letter : str) -> bool:
+        for row in polybius_square:
+            if letter in row:
+                return True
+        return False
+        
             
