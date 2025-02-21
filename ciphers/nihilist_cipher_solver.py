35f00dee2fb7108ee2cc3b7730121a43b7afd1e4
Yunus Hossain <yonusprince@gmail.com>
2025-02-21 10:50:32 -0500

    fixed polybius key function

--- ciphers/nihilist_cipher_solver.py
+++ ciphers/nihilist_cipher_solver.py
@@ -1,18 +1,17 @@
 from string import ascii_lowercase as lowercase_letters
 from cipher_solver import CipherSolver
 
-
 class NihilistSolver(CipherSolver):
     def __init__(self):
         super().__init__()
-
-    def encode(self, plaintext, keyword=None):
+    
+    def encode(self, plaintext, keyword = None):
         return super().encode(plaintext, keyword)
-
-    def decode(self, ciphertext, keyword=None):
+    
+    def decode(self, ciphertext, keyword = None):
         return super().decode(ciphertext, keyword)
-
-    def get_polybius_key(self, keyword: str) -> list:
+    
+    def get_polybius_key(self, keyword : str) -> list:
         polybius_square = [[], [], [], [], []]
         keyword_index_left_on = -1
         letters_index_left_on = 0
@@ -20,7 +19,7 @@ class NihilistSolver(CipherSolver):
             if len(row) == 5:
                 pass
             elif keyword_index_left_on != len(keyword) - 1:
-                for index, letter in enumerate(keyword[keyword_index_left_on + 1 :]):
+                for index, letter in enumerate(keyword[keyword_index_left_on + 1:]):
                     print(index, letter)
                     if not self.letter_is_in_polybius_square(polybius_square, letter):
                         row.append(letter)
@@ -30,9 +29,7 @@ class NihilistSolver(CipherSolver):
                         else:
                             pass
             if keyword_index_left_on == len(keyword) - 1:
-                for index, letter in enumerate(
-                    lowercase_letters[letters_index_left_on + 1 :]
-                ):
+                for index, letter in enumerate(lowercase_letters[letters_index_left_on + 1:]):
                     if not self.letter_is_in_polybius_square(polybius_square, letter):
                         row.append(letter)
                         letters_index_left_on = index
@@ -41,13 +38,11 @@ class NihilistSolver(CipherSolver):
                         else:
                             pass
         return polybius_square
-
-    def letter_is_in_polybius_square(self, polybius_square: list, letter: str) -> bool:
+            
+        
+    
+    def letter_is_in_polybius_square(self, polybius_square : list, letter : str) -> bool:
         for row in polybius_square:
             if letter in row:
                 return True
         return False
-
-
-a = NihilistSolver()
-print(a.get_polybius_key("flamingo"))
